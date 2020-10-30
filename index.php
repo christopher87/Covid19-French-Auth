c7c64b525062a1458aa9ee74ae7181fc373cd7d2
Christopher <christopher.bulhe@gmail.com>
2020-10-30 17:08:19 +0100

    fix

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Générateur attestation Covid</title>
+    <link rel="icon" type="image/png" href="favicon.png">
+    <style>
+        .form-group {
+            margin: 10px 0;
+        }
+        .form-group label {
+            display: block;
+        }
+        #generation-link {
+            margin: 20px 0;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <h2>
+            Créer votre lien personnalisé
+        </h2>
+
+        <p>
+            Remplissez le formulaire, et sauvegardez le lien généré qui vous sera proposé après validation.
+            <br>
+            A chaque lancement de ce lien, cela vous générera une attestation avec vos informations, à date et heure du moment de la génération.
+        </p>
+
+        <form id="form-profile">
+            <div class="form-group">
+                <label for="first-name">
+                    Prénom
+                </label>
+                <input type="text" id="first-name" name="firstname" autocomplete="given-name" placeholder="Jean" aria-labelledby="field-firstname-label" required="" autofocus="">
+            </div>
+            <div class="form-group">
+                <label for="last-name">
+                    Nom
+                </label>
+                <input type="text" id="last-name" name="lastname" autocomplete="family-name" placeholder="Dupont" aria-labelledby="field-lastname-label" required="" autofocus="">
+            </div>
+            <div class="form-group">
+                <label for="birthdate">
+                    Date de naissance
+                </label>
+                <input type="text" inputmode="numeric" id="birthdate" name="birthday" autocomplete="bday" placeholder="01/01/1970" maxlength="10" aria-labelledby="field-birthday-label" required="">
+            </div>
+            <div class="form-group">
+                <label for="birth-place">
+                    Lieu de naissance
+                </label>
+                <input type="text" id="birth-place" name="lieunaissance" aria-labelledby="field-lieunaissance-label" placeholder="Lyon" required="">
+            </div>
+            <div class="form-group">
+                <label for="address">
+                    Adresse
+                </label>
+                <input type="text" id="address" name="address" autocomplete="address-line1" aria-labelledby="field-address-label" placeholder="999 avenue de france" required="">
+            </div>
+            <div class="form-group">
+                <label for="city">
+                    Ville
+                </label>
+                <input type="text" id="city" name="town" autocomplete="address-level1" aria-labelledby="field-town-label" placeholder="Paris" required="">
+            </div>
+            <div class="form-group">
+                <label for="address">
+                    Code postal
+                </label>
+                <input type="number" min="00000" max="99999" id="post-code" name="post-code" autocomplete="zipcode" minlength="4" maxlength="5" aria-labelledby="field-zipcode-label" placeholder="75001" required="">
+            </div>
+            <div class="form-group">
+                <label for="motive">
+                    Raison
+                </label>
+                <select id="motive" name="motive">
+                    <option value="0">Sélectionnez une raison...</option>
+                    <option value="travail">Travail</option>
+                    <option value="achats">Achats</option>
+                    <option value="enfants">Enfants</option>
+                    <option value="sport_animaux">Sortie / Sport / Animaux</option>
+                    <option value="sante">Santé</option>
+                    <option value="famille">Famille</option>
+                    <option value="handicap">Handicap</option>
+                    <option value="convocation">Convocation</option>
+                    <option value="missions">Missions</option>
+                </select>
+            </div>
+        </form>
+        <button onclick="refreshData()">Valider</button>
+
+        <div id="generation-link" style="display: none;">
+            Votre lien :
+            <a href="generate.html"></a>
+        </div>
+
+        <p>
+            <small>
+                Aucune données n'est enregistrée ni exploitée.
+                <a href="https://github.com/christopher87/Covid19-French-Auth" target="_blank">github</a>.
+            </small>
+        </p>
+    </div>
+</body>
+<script src="demo.js?<?php echo date('YmdHis'); ?>"></script>
+</html>
