33ecf205198c4cc772b1e1c5a63dd2ec0a9cb98d
Christopher <christopher.bulhe@gmail.com>
2020-11-23 09:59:35 +0100

    update new version

--- generate.js
+++ generate.js
@@ -35,6 +35,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
 function badScriptLoading(event) {
     console.log("Official JS is not working. Patching using local copy.")
     let script = document.createElement('script');
-    script.src = "deplacement-covid-19/main.02381a6a.js";
+    script.src = "deplacement-covid-19/main.90bd2118.js";
     document.head.append(script)
 }
